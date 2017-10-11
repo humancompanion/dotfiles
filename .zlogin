19faca785bf50c1ec856389e371975cb9d2a1653
Matthew Dingee <matthew.dingee@opm.gov>
2017-10-11 12:09:40 -0400

    Updates to dotfiles post re-migration back to an old machine

---
+++ .zlogin
@@ -0,0 +1,2 @@
+/usr/local/bin/keychain id_rsa
+[[ -f ~/.keychain/acetate.local-sh ]] && source ~/.keychain/acetate.local-sh
--- brew-cask.sh
+++ brew-cask.sh
@@ -7,27 +7,25 @@
 
 # Install native apps
 
-brew install caskroom/cask/brew-cask
+# brew install caskroom/cask/brew-cask
 # brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
-brew cask install dropbox
-brew cask install gyazo
-brew cask install 1password
-brew cask install rescuetime
+# brew cask install spectacle
+# brew cask install dropbox
+# brew cask install gyazo
+# brew cask install 1password
+# brew cask install rescuetime
 # brew cask install flux
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
 brew cask install imagealpha
 brew cask install imageoptim
 
 # fun
-brew cask install limechat
+# brew cask install limechat
 # brew cask install miro-video-converter
-# brew cask install horndis               # usb tethering
+brew cask install horndis               # usb tethering
 
 # browsers
 # brew cask install google-chrome-canary
@@ -44,13 +42,8 @@ brew cask install gpgtools
 brew cask install licecap
 # brew cask install utorrent
 
-brew cask install spotify
+# brew cask install spotify
 
-brew tap caskroom/fonts 
-brew cask install font-fira-code
-
-
-# Not on cask but I want regardless.
-
-# File Multi Tool 5
-# Phosphor
+brew tap caskroom/fonts
+brew cask install font-source-sans-pro
+brew cask install font-merriweather
